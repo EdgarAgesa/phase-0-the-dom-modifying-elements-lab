8f7795621540c9406927fcd8aae21cc19b297430
Edgar <edgarasige@gmail.com>
2024-03-27 23:24:41 +0300

    Completed assingments

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main =document.querySelector('main#main')
+main.remove('main#main')
+
+const newHeader = document.createElement("h1");
+
+newHeader.id="victory"
+
+newHeader.textContent="Edgar is the champion"
